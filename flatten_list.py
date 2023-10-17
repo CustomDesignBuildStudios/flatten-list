3dce47cbdeb55f47fb5945002e71bb92f9b3e3c3
Michael Termotto <mjtermotto11@gmail.com>
2023-10-18 00:02:02 +0200

    Added function

--- flatten_list.py
+++ flatten_list.py
@@ -1,2 +1,11 @@
 def flatten_list(lst):
-    pass
+    arr = []
+    for item in lst:
+        if isinstance(item, list):
+            arr.extend(flatten_list(item))
+        else:
+            arr.append(item)
+    return arr
+
+print(flatten_list([1, [2], [3, [4, 5, [6]]], 7]))
+
